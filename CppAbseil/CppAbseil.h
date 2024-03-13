515e7ebc6054229e74fa566cc317070a0ad66632
Leundo <daysundoing@outlook.com>
2024-03-13 23:00:40 +0800

    Initial Commit

---
+++ CppAbseil/CppAbseil.h
@@ -0,0 +1,18 @@
+//
+//  CppAbseil.h
+//  CppAbseil
+//
+//  Created by Undo Hatsune on 2024/03/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CppAbseil.
+FOUNDATION_EXPORT double CppAbseilVersionNumber;
+
+//! Project version string for CppAbseil.
+FOUNDATION_EXPORT const unsigned char CppAbseilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CppAbseil/PublicHeader.h>
+
+
